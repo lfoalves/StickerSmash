3e7d116a079d8baa64aa298854522233e6a80d10
lfoalves <lfoalves.dev@gmail.com>
2022-12-11 13:57:01 -0400

    first commit

---
+++ utils/imagesRandom.js
@@ -0,0 +1,8 @@
+export const imagesRandom = [
+  'https://source.unsplash.com/random?a=1',
+  'https://source.unsplash.com/random?b=1',
+  'https://source.unsplash.com/random?c=1',
+  'https://source.unsplash.com/random?d=1',
+  'https://source.unsplash.com/random?e=1',
+  'https://source.unsplash.com/random?f=1'
+]
